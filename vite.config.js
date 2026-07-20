b85bad8abfa2bd566c2163fbe6a8f1126353436a
shriii19 <mudabeshrinivas@gmail.com>
2026-07-20 22:50:00 +0400

    feat: update Vite configuration to enhance chunking strategy for better performance

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 900,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("react-dom") || id.includes("react")) return "react";
+          if (id.includes("@react-three/postprocessing") || id.includes("postprocessing")) {
+            return "postfx";
+          }
+          if (
+            id.includes("three") ||
+            id.includes("@react-three/fiber") ||
+            id.includes("@react-three/drei")
+          ) {
+            return "three";
+          }
+          return undefined;
+        },
+      },
+    },
+  },
 })
